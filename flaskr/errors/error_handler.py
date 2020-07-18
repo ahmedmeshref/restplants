3d788d269e99a095bd5f18f71a12ef62dd430be1
ahmedmeshref <a.meshref@alustudent.com>
2020-07-18 02:15:31 +0200

    Add return JSON from errors & add implementation of new error_handelers

--- flaskr/errors/error_handler.py
+++ flaskr/errors/error_handler.py
@@ -1,13 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 errors = Blueprint('errors', __name__)
 
 
+# note: errors and success responses needs to be consistence, format JSON
 @errors.app_errorhandler(404)
-def handle_404(error):
-    return "Not found! once I am initialized, will let you know. Stay awesome!"
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Page not found! once I am initialized, will let you know. Stay awesome!'
+    }), 404
 
 
 @errors.app_errorhandler(405)
-def handle_notAllowed(error):
-    return "I am not Allowed yet! once they create me, I will let you know. Stay awesome!"
+def not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': "Page not allowed! once I am initialized, will let you know. Stay awesome!"
+    }), 405
+
+
+@errors.app_errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': "Unprocessable request! please review your request body and format. Stay awesome!"
+    }), 422
+
+
+@errors.app_errorhandler(500)
+def server_damage(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': "Internal server error! try to send your request again. Stay awesome!"
+    }), 500
